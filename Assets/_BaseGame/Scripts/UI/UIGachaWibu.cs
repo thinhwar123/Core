847d33b58536cff58a971095e392b66a8d4775db
thinhwar123 <52587916+thinhwar123@users.noreply.github.com>
2023-09-30 16:33:19 +0700

    Update Asset

--- Assets/_BaseGame/Scripts/UI/UIGachaWibu.cs
+++ Assets/_BaseGame/Scripts/UI/UIGachaWibu.cs
@@ -6,4 +6,29 @@ using UnityEngine;
 public class UIGachaWibu : AUIPanel
 {
     [field: SerializeField] public UICharacterInfo UICharacter {get; private set;}
+    [field: SerializeField] public AUIButton ScoutButton {get; private set;}
+    [field: SerializeField] public AUIButton BigScoutButton {get; private set;}
+    [field: SerializeField] public AUIButton ButtonClose {get; private set;}
+    
+    protected override void Awake()
+    {
+        base.Awake();
+        ScoutButton.OnClickButton.AddListener(OnClickButtonScout);
+        BigScoutButton.OnClickButton.AddListener(OnClickButtonBigScout);
+        ButtonClose.OnClickButton.AddListener(OnClickButtonClose);
+    }
+
+    public void OnClickButtonScout()
+    {
+        Debug.Log("Scout");
+    }
+    public void OnClickButtonBigScout()
+    {
+        Debug.Log("BigScout");
+    }
+
+    public void OnClickButtonClose()
+    {
+        ClosePanel<UIGachaWibu>();
+    }
 }
